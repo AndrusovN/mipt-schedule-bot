3c0b2eea4611efdaa6f781b315472c1589a33ade
AndrusovN <andrusov.n@gmail.com>
2022-09-02 22:27:38 +0300

    initial commit

---
+++ bot.py
@@ -0,0 +1,85 @@
+import telebot
+import threading
+import time
+from update import update_schedules, days
+import datetime
+
+
+# Создаем экземпляр бота
+bot = telebot.TeleBot('5600930557:AAErz9Erf1tDF4vixQNaEWP3tJ-bFfNwHt8')
+users = {}
+admin_chat_id = 228041096
+
+
+@bot.message_handler(commands=['report'])
+def report(m):
+    bot.send_message(admin_chat_id, "Bug report from user:\n" + m.from_user.username + "\n" + m.text[8:])
+
+
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m):
+    bot.send_message(m.chat.id, f'''Привет, {m.from_user.first_name}! 
+Я бот с расписанием МФТИ. Пока расписание работает только для первого курса
+Напиши /setgroup <твоя группа> без кавычек, чтобы тебе приходила актуальная информация о приближающихся парах''')
+
+
+@bot.message_handler(commands=["setgroup"])
+def set_group(m):
+    parts = m.text.split(' ')
+    if len(parts) != 2:
+        bot.send_message(m.chat.id, f'Пожалуйста, после команды введите название группы в том же сообщении')
+        return
+    if parts[1][0] != "Б":
+        bot.send_message(m.chat.id, f'Пожалуйста, введите корректное название группы')
+        return
+    if m.chat.id not in users.keys():
+        print(f"New user joined!!!\n{m.from_user.username}")
+    users[m.chat.id] = parts[1]
+    bot.send_message(m.chat.id, f'Вы задали группу: {parts[1]}')
+
+
+@bot.message_handler(content_types=['text'])
+def easter_egg(m):
+    if m.text == 'РТ':
+        bot.send_message(m.chat.id, 'РТ!!!\n\n'*20)
+    else:
+        bot.send_message(m.chat.id, '''Прости, я пока не умею отвечать на такой запрос(
+Если ты считаешь, что в работе бота что-то не так, то напиши команду /report <описание проблемы> без кавычек''')
+
+
+def notify(group_name, timestamp, lesson):
+    for user in users.keys():
+        if users[user] == group_name:
+            bot.send_message(user, f"В {timestamp} начинается занятие:\n\n{lesson}")
+
+
+def everyday_update():
+    print("updating")
+    today_schedule = update_schedules()
+    print("schedule received")
+    today = datetime.date.today().weekday()
+    today = days[today - 5]
+    now = datetime.datetime.now()
+    today_str = str(datetime.date.today())[:10] + ' '
+    for group in today_schedule.keys():
+        current = today_schedule[group][today]
+        for lesson in current:
+            daytime = today_str + lesson[0] + ":00"
+            runtime = datetime.datetime.strptime(daytime, "%Y-%m-%d %H:%M:%S")
+            delay = (runtime - now).total_seconds() - 15*60
+            if delay > 0:
+                threading.Timer(delay, notify, (group, lesson[0], lesson[1])).start()
+    next_update = str(datetime.date.today() + datetime.timedelta(days=1))[:10] + " 08:30:00"
+    next_update = datetime.datetime.strptime(next_update, "%Y-%m-%d %H:%M:%S")
+    delay = (next_update - datetime.datetime.now()).total_seconds()
+    threading.Timer(delay, everyday_update).start()
+    print(f"finished updating - next update in {delay // 60} minutes")
+
+
+bot_thread = threading.Thread(target=bot.polling, kwargs={'none_stop': True, 'interval': 0})
+bot_thread.start()
+everyday_update()
+print("Success")
+while True:
+    time.sleep(1)
---
+++ update.py
@@ -0,0 +1,95 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import urllib.request
+import os
+import xlrd
+
+url = 'https://mipt.ru/about/departments/uchebniy/schedule/study/'
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+filename = "schedule.xls"
+
+
+def remake_hours(hours):
+    if type(hours) == str:
+        hours = hours.split(' - ')[0]
+        hours = hours[:-2] + ":" + hours[-2:]
+    return hours
+
+
+def make_schedule(group_name, hours, lessons):
+    pairs = []
+    for i in range(min(len(hours), len(lessons[group_name]))):
+        if type(hours[i]) == str:
+            pairs.append((hours[i], lessons[group_name][i]))
+
+    schedule = {}
+    current = []
+    id = 0
+    for item in pairs:
+        if item[0] == '9:00' and len(current) != 0:
+            schedule[days[id]] = current
+            current = []
+            id += 1
+        if type(item[1]) == str:
+            current.append(item)
+    schedule[days[id]] = current
+    return schedule
+
+
+def receive_excel_file():
+    data = requests.get(url)
+    bs = BeautifulSoup(data.text)
+    result = bs.find(lambda tag: tag.name == "li" and "1 курс бакалавриата, специалитета" in tag.text)
+    ref = result.find("a")['href']
+
+    if os.path.exists(filename):
+        os.remove(filename)
+    else:
+        print("The file does not exist")
+
+    urllib.request.urlretrieve("https://mipt.ru" + ref, filename)
+
+
+def read_excel_file():
+    ExcelFile = pd.read_excel(filename)
+    xl = xlrd.open_workbook(filename, formatting_info=True)
+    FirstSheet = xl.sheet_by_index(0)
+    for crange in FirstSheet.merged_cells:
+        # print("there")
+        rlo, rhi, clo, chi = crange
+        for rowx in range(rlo, rhi):
+            for colx in range(clo, chi):
+                value = FirstSheet.cell(rowx, colx).value
+                if len(value) == 0:
+                    try:
+                        ExcelFile.iloc[rowx - 1, colx - 1] = FirstSheet.cell(rlo, clo).value
+                    except:
+                        pass
+
+    df = ExcelFile
+    df.columns = df.iloc[3]
+    return df
+
+
+def parse_lesson_hours():
+    df = pd.read_excel(io="schedule.xls", sheet_name="1 курс")
+    df.columns = df.iloc[3]
+    hours = df['Часы'].iloc[:, 0].tolist()
+    hours = hours[4:]
+
+    return [remake_hours(x) for x in hours]
+
+
+def update_schedules():
+    receive_excel_file()
+
+    df = read_excel_file()
+
+    group_names = list(filter(lambda x: type(x) == str and x[0] == 'Б', df.columns.tolist()))
+    lessons = {group_name: df[group_name].tolist()[4:] for group_name in group_names}
+
+    hours = parse_lesson_hours()
+
+    schedules = {group_name: make_schedule(group_name, hours, lessons) for group_name in group_names}
+    return schedules
