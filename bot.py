839ae7a9cb8531f1b1f8e6f22c515b80553f353b
AndrusovN <andrusov.n@gmail.com>
2022-09-03 10:15:03 +0300

    fix groups and unsubscribe option

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from update import update_schedules, days
 import datetime
 import json
 import os
+import re
 
 
 # Создаем экземпляр бота
@@ -12,12 +13,16 @@ bot = telebot.TeleBot(input('Введите токен бота:\n'))
 users = {}
 admin_chat_id = 228041096
 users_filename = "users.json"
+users_lock = threading.Lock()
+group_name_pattern = re.compile("Б[0-9][0-9]-[0-9][0-9][0-9]$")
 
 
 def upload_users():
     global users
+    users_lock.acquire()
     if not os.path.exists(users_filename):
         users = {}
+        users_lock.release()
         return
     with open(users_filename, 'r') as f:
         data = f.read()
@@ -27,11 +32,14 @@ def upload_users():
         except:
             print("Trouble with reading users")
             users = {}
+    users_lock.release()
 
 
 def save_users():
+    users_lock.acquire()
     with open(users_filename, 'w') as f:
         f.write(json.dumps(users))
+    users_lock.release()
 
 
 @bot.message_handler(commands=['report'])
@@ -45,16 +53,28 @@ def start(m):
     bot.send_message(m.chat.id, f'''Привет, {m.from_user.first_name}! 
 Я бот с расписанием МФТИ. Пока расписание работает только для первого курса
 Напиши /setgroup <твоя группа> без кавычек, чтобы тебе приходила актуальная информация о приближающихся парах
-Можешь написать /info, чтобы узнать, в какой группе ты зарегистрирован в боте''')
+Можешь написать /info, чтобы узнать, в какой группе ты зарегистрирован в боте
+Чтобы отписаться от уведомлений, напиши /unsubscribe''')
+
+
+@bot.message_handler(commands=['unsubscribe'])
+def unsubscribe(m):
+    users_lock.acquire()
+    if m.chat.id in users.keys():
+        users.pop(m.chat.id)
+    bot.send_message(m.chat.id, 'Теперь вы не будете получать уведомления(')
+    users_lock.release()
 
 
 @bot.message_handler(commands=['info'])
 def info(m):
+    users_lock.acquire()
     if m.chat.id in users.keys():
         bot.send_message(m.chat.id, f'Вы отслеживаете группу {users[m.chat.id]}')
     else:
         bot.send_message(m.chat.id, '''Пока вы не отслеживаете никакую группу(
 Напишите /setgroup <ваша группа> без кавычек, чтобы получать уведомления о приближающихся парах''')
+    users_lock.release()
 
 
 @bot.message_handler(commands=["setgroup"])
@@ -63,14 +83,19 @@ def set_group(m):
     if len(parts) != 2:
         bot.send_message(m.chat.id, f'Пожалуйста, после команды введите название группы в том же сообщении')
         return
-    if parts[1][0] != "Б":
-        bot.send_message(m.chat.id, f'Пожалуйста, введите корректное название группы')
+    if not re.match(group_name_pattern, parts[1]):
+        bot.send_message(m.chat.id, f'Название группы должно быть вида БXY-PQR')
         return
+    print("going to save")
+    users_lock.acquire()
     if m.chat.id not in users.keys():
         print(f"New user joined!!!\nusername: {m.from_user.username}, id: {m.from_user.id}")
 
     users[m.chat.id] = parts[1]
+
+    users_lock.release()
     save_users()
+    print("saved")
     bot.send_message(m.chat.id, f'Вы задали группу: {parts[1]}')
 
 
@@ -84,12 +109,16 @@ def easter_egg(m):
 
 
 def notify(group_name, timestamp, lesson):
+    users_lock.acquire()
     for user in users.keys():
+        print(user, users[user], group_name, users[user] == group_name)
         if users[user] == group_name:
             bot.send_message(user, f"В {timestamp} начинается занятие:\n\n{lesson}")
+    users_lock.release()
 
 
 def everyday_update():
+    users_lock.acquire()
     print("updating")
     today_schedule = update_schedules()
     print("schedule received")
@@ -97,18 +126,21 @@ def everyday_update():
     today = days[today - 5]
     now = datetime.datetime.now()
     today_str = str(datetime.date.today())[:10] + ' '
-    for group in today_schedule.keys():
+    for i, group in enumerate(today_schedule.keys()):
         current = today_schedule[group][today]
         for lesson in current:
             daytime = today_str + lesson[0] + ":00"
             runtime = datetime.datetime.strptime(daytime, "%Y-%m-%d %H:%M:%S")
-            delay = (runtime - now).total_seconds() - 15*60
+            delay = (runtime - now).total_seconds() - 15*60 - i
+            if group == 'Б05-228':
+                print(delay)
             if delay > 0:
                 threading.Timer(delay, notify, (group, lesson[0], lesson[1])).start()
     next_update = str(datetime.date.today() + datetime.timedelta(days=1))[:10] + " 08:30:00"
     next_update = datetime.datetime.strptime(next_update, "%Y-%m-%d %H:%M:%S")
     delay = (next_update - datetime.datetime.now()).total_seconds()
     threading.Timer(delay, everyday_update).start()
+    users_lock.release()
     print(f"finished updating - next update in {delay // 60} minutes")
 
 
